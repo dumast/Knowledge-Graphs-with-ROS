1974460970a46517ae82718c9d200ef6b3d5742b
Terence Dumas <dumast@cs.utexas.edu>
2024-05-05 17:59:35 -0500

    added client

--- duke_silver/getPosition.py
+++ duke_silver/getPosition.py
@@ -1,11 +1,14 @@
 import roslibpy
-import roslibpy.tf.TFClient
+import roslibpy.tf
 
 def update_position(current_pos):
     global position
     position = current_pos
     print("POSITION:")
     print(position)
+
+client = roslibpy.Ros(host="0.0.0.0", port=9090)
+client.run()
 
 tf_client = roslibpy.tf.TFClient(client, "/2ndFloorWhole_map")
 tf_client.subscribe("base_link", update_position)
